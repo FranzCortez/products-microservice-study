815ab16475e5f442624d58fed794f438165faab6
franz.cortez <franz.cortez@ubq.cl>
2024-11-26 22:01:36 -0300

    Microservice with NATS and docker

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+import { envs } from './config/envs';
+
+async function bootstrap() {
+  const logger = new Logger('ms-products');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServer,
+      },
+    },
+    // {
+    //   transport: Transport.TCP,
+    //   options: {
+    //     port: envs.port,
+    //   },
+    // },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen();
+
+  logger.log(`Microservice is running on port ${envs.port}`);
+}
+bootstrap();
